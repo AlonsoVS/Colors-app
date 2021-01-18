9c2f1bfd7854689ab96790b3189f4728b721df99
alonso <alonsovillegas2016@gmail.com>
2021-01-18 17:46:51 -0500

    Add Color Helper to generate colors

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -9,7 +9,7 @@ class ColorBox extends Component {
     
     handleCopy = () => {
         this.setState({ copied: true }, (() => {
-            setTimeout(() => this.setState({ copied: false }), 3000);
+            setTimeout(() => this.setState({ copied: false }), 1500);
         }))
     }
 
