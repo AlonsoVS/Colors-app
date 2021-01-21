562bf114e77043c85cec0b5536c681dd7cf07aae
alonso <alonsovillegas2016@gmail.com>
2021-01-20 23:16:07 -0500

    Change styles to a separate directory

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -4,47 +4,7 @@ import { Link } from 'react-router-dom';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
-
-const styles = {
-    Palette: {
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column"
-    },
-    colors: {
-        height: "90%"
-    },
-    goBack: {
-        background: "black",
-        width: "20%",
-        height: "50%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-4px",
-        "& a": {
-            color: "white",
-            width: "100px",
-            height: "30px",
-            position: "absolute",
-            display: "inline-block",
-            top: "50%",
-            left: "50%",
-            marginLeft: "-50px",
-            marginTop: "-15px",
-            textAlign: "center",
-            outline: "none",
-            background: "rgba(225, 225, 225, 0.3)",
-            fontSize: "1rem",
-            lineHeight: "30px",
-            color: "white",
-            textTransform: "uppercase",
-            border: "none",
-            textDecoration: "none",
-        }
-    }
-}
+import styles from './styles/PaletteStyles';
 
 class SingleColorPalette extends Component {
 
